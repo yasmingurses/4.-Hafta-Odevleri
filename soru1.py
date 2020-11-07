f878588830df0d8420add3d7aa630eea9b8bfb36
Yasmin Gürses <72697857+yasmingurses@users.noreply.github.com>
2020-11-07 18:34:12 +0300

    Add files via upload

---
+++ soru1.py
@@ -0,0 +1,17 @@
+# Belirli listedeki elemanların toplamını bulmak için çalışıyor ancak kullanıcıdan başka elemanlı liste yaratması istenince toplamı bulmayı yapamadım
+
+#Listedeki elemanların toplamını bulmak
+
+lam = 0
+  
+liste = [ 3 , 4 , 6 ,7 , 10 , 1, 50 ]  
+  
+for i in range(0, len(liste)): 
+    toplam = toplam + liste[i] 
+  
+print("Listedeki elemanların toplamı: ", toplam) 
+
+
+
+
+
